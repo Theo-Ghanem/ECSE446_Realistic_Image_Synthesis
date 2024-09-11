82646466aa53c46069cb9ee68920311e6a3b966b
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2024-09-10 23:37:56 -0400

    A1 looks okay but image is mirrored

---
+++ A1/taichi_tracer/camera.py
@@ -0,0 +1,162 @@
+import taichi as ti
+import taichi.math as tm
+import numpy as np
+
+from .ray import Ray
+
+
+@ti.data_oriented
+class Camera:
+
+    def __init__(self, width: int = 128, height: int = 128) -> None:
+
+        # Camera pixel width and height are fixed
+        self.width = width
+        self.height = height
+
+        # Camera parameters that can be modified are stored as fields
+        self.eye = ti.Vector.field(n=3, shape=(), dtype=float)
+        self.at = ti.Vector.field(n=3, shape=(), dtype=float)
+        self.up = ti.Vector.field(n=3, shape=(), dtype=float)
+        self.fov = ti.field(shape=(), dtype=float)
+
+        self.x = ti.Vector.field(n=3, shape=(), dtype=float)
+        self.y = ti.Vector.field(n=3, shape=(), dtype=float)
+        self.z = ti.Vector.field(n=3, shape=(), dtype=float)
+
+        self.camera_to_world = ti.Matrix.field(n=4, m=4, shape=(), dtype=float)
+
+        # Initialize with some default params
+        self.set_camera_parameters(
+            eye=tm.vec3([0, 0, 5]),
+            at=tm.vec3([0, 0, 0]),
+            up=tm.vec3([0, 1, 0]),
+            fov=60.
+            )
+
+
+    def set_camera_parameters(
+        self, 
+        eye: tm.vec3 = None, 
+        at: tm.vec3 = None, 
+        up: tm.vec3 = None, 
+        fov: float = None
+        ) -> None:
+
+        if eye: self.eye[None] = eye
+        if at: self.at[None] = at
+        if up: self.up[None] = up
+        if fov: self.fov[None] = fov
+        self.compute_matrix()
+
+
+    @ti.kernel
+    def compute_matrix(self):
+
+        '''
+        TODO: Compute Camera to World Matrix
+
+        self.camera_to_world[None] = tm.mat4(<You Matrix>)
+
+        '''
+
+        # Compute the camera coordinate frame
+        z = (self.eye[None] - self.at[None]).normalized()
+        x = self.up[None].cross(z).normalized()
+        y = z.cross(x)
+
+        # Compute the camera to world matrix
+        # self.camera_to_world[None] = tm.mat4([
+        #     [x[0], x[1], x[2], -x.dot(self.eye[None])],
+        #     [y[0], y[1], y[2], -y.dot(self.eye[None])],
+        #     [z[0], z[1], z[2], -z.dot(self.eye[None])],
+        #     [0, 0, 0, 1]
+        # ])
+
+        self.camera_to_world[None] = tm.mat4([
+            [x[0], y[0], z[0], -x.dot(self.eye[None])],
+            [x[1], y[1], z[1], -y.dot(self.eye[None])],
+            [x[2], y[2], z[2], -z.dot(self.eye[None])],
+            [0, 0, 0, 1]
+        ])
+
+
+    @ti.func
+    def generate_ray(self, pixel_x: int, pixel_y: int, jitter: bool = False) -> Ray:
+        
+        '''
+        TODO: Generate Ray
+
+        - generate ndc coords
+        - generate camera coods from NDC coords
+        - generate a ray
+            - ray = Ray()
+        - set the ray direction and origin
+            - ray.origin = ...
+            - ray.direction = ...
+        - Ignore jittering for now
+        - return ray
+        '''
+
+        #Generate ndc coords:
+        ndc_coords = self.generate_ndc_coords(pixel_x, pixel_y, jitter)
+        # print ("ndc_coords : ", ndc_coords)
+
+        #generate camera coords from NDC coords:
+        camera_coords = self.generate_camera_coords(ndc_coords)
+        # print ("camera_coords : ", camera_coords)
+
+        ray = Ray()
+
+        # set the ray direction and origin
+        ray.origin = self.eye[None]
+        ray.direction = (self.camera_to_world[None] @ camera_coords).xyz.normalized()
+
+        return ray
+
+
+    @ti.func
+    def generate_ndc_coords(self, pixel_x: int, pixel_y: int, jitter: bool = False) -> tm.vec2:
+        
+        '''
+        TODO: Generate NDC coods
+
+        - Ignore jittering for now
+        
+        return tm.vec2([ndc_x, ndc_y])
+
+        '''
+        # Convert pixel coordinates to float
+        pixel_x = ti.cast(pixel_x + 0.5, ti.f32)
+        pixel_y = ti.cast(pixel_y + 0.5, ti.f32)
+
+        # To convert pixel coordinates to NDC coordinates, we first bring it from [0, width]
+        # to [0, 1] and then to [-1, 1] as described in tutorial 1
+        ndc_x = (2.0 * pixel_x - self.width) / self.width
+        ndc_y = (2.0 * pixel_y - self.height) / self.height
+
+        return tm.vec2([ndc_x, ndc_y])
+
+    @ti.func
+    def generate_camera_coords(self, ndc_coords: tm.vec2) -> tm.vec4:
+        
+        '''
+        TODO: Generate Camera coordinates
+        - compute camera_x, camera_y, camera_z
+        - return tm.vec4([camera_x, camera_y, camera_z, 0.0])
+        '''
+        #Generate Camera coordinates
+        # ndc_x = ndc_coords[0]
+        # ndc_y = ndc_coords[1]
+        #
+        # cam_x = ndc_x * self.x[None][0] + ndc_y * self.y[None][0] + self.z[None][0]
+        # cam_y = ndc_x * self.x[None][1] + ndc_y * self.y[None][1] + self.z[None][1]
+        # cam_z = ndc_x * self.x[None][2] + ndc_y * self.y[None][2] + self.z[None][2]
+        #
+        # return tm.vec4([cam_x, cam_y, cam_z, 0.0])
+        aspect_ratio = self.width / self.height
+        scale = ti.tan(self.fov[None] * 0.5 * np.pi / 180)
+        cam_x = ndc_coords[0] * aspect_ratio * scale
+        cam_y = ndc_coords[1] * scale
+        cam_z = -1.0
+        return tm.vec4([cam_x, cam_y, cam_z, 0.0]) #homogeneous coordinates o for dir vector, 1 for pt vector
---
+++ A1/taichi_tracer/ray_intersector.py
@@ -0,0 +1,118 @@
+from abc import ABC, abstractmethod
+
+import taichi as ti
+import taichi.math as tm
+
+from .geometry import Geometry
+from .materials import Material
+from .ray import Ray, HitData
+
+
+@ti.data_oriented
+class RayIntersector(ABC):
+
+    def __init__(self, geometry: Geometry):
+        self.EPSILON = 1e-7
+        self.geometry = geometry
+
+
+    @abstractmethod
+    @ti.func
+    def query_ray(ray: Ray) -> HitData:
+        pass
+
+
+    @ti.func
+    def intersect_triangle(self, ray: Ray, triangle_id: int) -> HitData:
+
+        hit_data = HitData()
+
+        # Grab Vertices
+        vert_ids = self.geometry.triangle_vertex_ids[triangle_id-1] - 1  # Vertices are indexed from 1
+        v0 = self.geometry.vertices[vert_ids[0]]
+        v1 = self.geometry.vertices[vert_ids[1]]
+        v2 = self.geometry.vertices[vert_ids[2]]
+
+        # Normals at each vertex
+        normal_indices = self.geometry.triangle_normal_ids[triangle_id-1]-1
+
+        normal_0 = self.geometry.normals[normal_indices[0]]
+        normal_1 = self.geometry.normals[normal_indices[1]]
+        normal_2 = self.geometry.normals[normal_indices[2]]
+
+        # Material of the triangle
+        material_id = self.geometry.triangle_material_ids[triangle_id-1]
+
+
+        '''
+        TODO: Ray-Triangle intersection
+        
+        - v0, v1, v2 are the vertices of a given triangle
+        - normal_0, normal_1, normal2 are the normals at each vertex
+
+        # check if the ray intersects inside the given triangle
+        # if it is, fill the hit_data with the following information
+        '''
+
+        #1st compute edge vectors:
+        e1 = v1 - v0
+        e2 = v2 - v0
+
+        det = tm.cross(ray.direction, e2).dot(e1)
+
+        if -self.EPSILON < det < self.EPSILON: #ray is parallel to the triangle
+            hit_data.is_hit = False
+        else: # it intersects the triangle, inside or outside?
+            #compute the barycentric coordinates:
+            u = (ray.origin - v0).dot(tm.cross(ray.direction, e2))/det
+            v = (ray.direction.dot(tm.cross((ray.origin-v0), e1))) / det
+            w = 1.0 - u - v
+
+            # We then verify whether the barycentric coordinates fall within the sheared triangle frame bounds:
+            if 0 <= u <= 1 and 0 <= v <= 1 and u+v <= 1: # If all these tests pass, then the ray intersects the triangle at parametric distance
+                t = (e2.dot(tm.cross((ray.origin - v0), e1)))/det
+
+                #To account for numerics, only consider intersections for 𝑡 > 𝜖
+                if t < self.EPSILON: #check if the intersection is behind the ray
+                    hit_data.is_hit = False
+
+                #check if the intersection is closer than the current closest intersection
+
+                else: # We compute the intersection point:
+                    hit_data.is_hit = True
+                    #a flag that is true if the triangle is backfacing (hint: check the sign of the determinant),
+                    hit_data.is_backfacing = det < 0
+                    hit_data.triangle_id = triangle_id
+                    hit_data.distance = (ray.origin - v0).dot(tm.cross(e1, e2)) / det
+                    hit_data.barycentric_coords = tm.vec2(u, v)
+                    #the per-pixel interpolated normal, returned as a taichi.math vec3 type (hint: you will need to flip your normal if your triangle is backfacing, as well as performing the per-vertex to per-pixel interpolation and renormalization)
+                    if hit_data.is_backfacing:
+                        hit_data.normal = w*normal_0 + u*normal_1 + v*normal_2
+                    else:
+                        hit_data.normal = w*normal_0 + v*normal_1 + u*normal_2
+                    hit_data.material_id = material_id
+
+
+
+        return hit_data
+
+
+@ti.data_oriented
+class BruteForceRayIntersector(RayIntersector):
+
+    def __init__(self, geometry: Geometry) -> None:
+        super().__init__(geometry)
+
+
+    @ti.func
+    def query_ray(self, ray: Ray) -> HitData:
+
+        closest_hit = HitData()
+        for triangle_id in range(1, self.geometry.n_triangles + 1):
+            hit_data = self.intersect_triangle(ray, triangle_id)
+
+            if hit_data.is_hit:
+                if (hit_data.distance < closest_hit.distance) or (not closest_hit.is_hit):
+                    closest_hit = hit_data
+
+        return closest_hit
